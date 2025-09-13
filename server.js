26224b417a7f90e0df0a26913907ee4d91f3ca97
2403717620521043-lab <2403717620521043@cit.edu.in>
2025-09-13 06:40:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -271,7 +271,7 @@ io.on('connection', (socket) => {
 });
 
 // Use the port provided by Render, or default to 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
